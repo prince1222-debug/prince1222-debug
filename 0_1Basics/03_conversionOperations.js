8c8121cfe4b5935ecfd5c723d5121ae4938de829
prince1222-debug <boisfromhell@gmail.com>
2024-12-03 16:01:43 +0000

    conversion on datatypes

---
+++ 0_1Basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let game = "mario"
+let score = '50'
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber);
+
+
+//"50asf" = NaN not a Number
+//null = 0
+//booleans True = 1
+//booleans False = 0
+
+let isLogin = 1
+let booleansisLoggedin = Boolean(isLogin);
+console.log(booleansisLoggedin);
+
+let someNumber = 23
+let stringNumber = String(someNumber);
+console.log(stringNumber);
