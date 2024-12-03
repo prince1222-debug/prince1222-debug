71ba3300d84116f2d15b49559f88db7d0b166fc8
prince1222-debug <boisfromhell@gmail.com>
2024-12-03 14:54:08 +0000

    datatypes

---
+++ 0_1Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use Strict"; // Treat all code as a newer version of JavaScript
+
+// alert("Hello") We are using nodejs not browser //
+
+console.log(3+3);
+
+console.log("Prince");
+
+let name = "prince" // string datatypes
+let age = 20 // Number Datatypes
+let isoggedIn = true // booleans
+
+// number = 2 to power of 53
+// booleans = true and false
+// bigint 
+// null = standalone value (empty value)
+// undefined = not defined of a value
+// symbol = unique
+
+
+// object
+
+console.log(typeof "prince");
+console.log(typeof true);
+console.log(typeof null); // it is an object 
+console.log(typeof undefined); // it is an type undefined
+
