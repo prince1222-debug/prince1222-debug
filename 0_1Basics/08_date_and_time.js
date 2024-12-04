54f241bce1ab053bc4290059f39801ea98b8fcd9
prince1222-debug <boisfromhell@gmail.com>
2024-12-04 13:14:48 +0000

    date and time

---
+++ 0_1Basics/08_date_and_time.js
@@ -0,0 +1,27 @@
+// Dates
+
+// const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// const mycreatedDate = new Date(2024, 1, 29)
+// const mycreatedDate = new Date("2024-02-29");
+// const mycreatedDate = new Date("02-29-2024");
+// console.log(mycreatedDate.toLocaleString())
+
+// let mytimeStamps = Date.now();
+// console.log(mytimeStamps);
+// console.log((mycreatedDate.getTime()));
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date();
+// // console.log(newDate.getMonth());
+// // console.log(newDate.getDate());
+// // console.log(newDate.getDay());
+
+// newDate.toLocaleString('default', { 
+//     weekday: "long"
+// })
