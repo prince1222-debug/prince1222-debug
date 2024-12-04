99b2437e49d6db7c22edfe2e964e6e8d7dff07ca
prince1222-debug <boisfromhell@gmail.com>
2024-12-04 07:56:31 +0000

    datatypes-explain and variables

---
+++ 0_1Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = "12412"
+let accountEmail = "prince34@log.com"
+var accountPassword = "123442"
+accountCity = "Raipur"
+let accountState;
+
+console.log(accountId);
+
+/* Prefer not to use var */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 0_1Basics/05_datatypes-explain.js
@@ -0,0 +1,36 @@
+// Primitive datatypes 
+
+// 7 types : Strings, Number, Booleans, mull, undefined, Symbol, BigInt.
+
+// const value = 100
+// const scoreValue = 100.5
+
+// const isloogedIn = true
+// const temperature = null
+// 
+// let useremail;
+
+// let id = Symbol("123")
+// let anotherId = Symbol("123")
+// console.log(id === anotherId);
+// console.log(id, anotherId)
+
+// const bigNumber = 48568972349273947n // after number use n and it will converted into bigInt
+
+
+// Reference Datatypes or Non Primitive 
+
+// Arrays, Objects, Functions
+
+
+// const heroes = ["shaktiman", "Hanuman", "Krrish"]
+
+// let myObj = {
+//     name:"prince",
+//     age:23,
+//     city:"Jandaha"
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World")
+// }
