16cb86f4edcb86b72fb476e4dfcd6a1a12e90532
prince1222-debug <boisfromhell@gmail.com>
2024-12-04 07:03:14 +0000

    conversion and comparisons

--- 0_1Basics/03_conversionOperations.js
+++ 0_1Basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
-let game = "mario"
-let score = '50'
+// let game = "mario"
+// let score = '50'
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valueinNumber = Number(score);
-console.log(typeof valueinNumber);
+// let valueinNumber = Number(score);
+// console.log(typeof valueinNumber);
 
 
 //"50asf" = NaN not a Number
@@ -13,10 +13,44 @@ console.log(typeof valueinNumber);
 //booleans True = 1
 //booleans False = 0
 
-let isLogin = 1
-let booleansisLoggedin = Boolean(isLogin);
-console.log(booleansisLoggedin);
+// let isLogin = 1
+// let booleansisLoggedin = Boolean(isLogin);
+// console.log(booleansisLoggedin);
 
-let someNumber = 23
-let stringNumber = String(someNumber);
-console.log(stringNumber);
+// let someNumber = 23
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+
+//       Operations      //
+
+// let value = 3
+// let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2*2);
+
+// let str1 = "hello"
+// let str2 = " prince"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(2 + 2 + "1");
+// console.log("12" + 3 );
+// console.log("1" + 2 + 2);
+// console.log( (2 + 23) * (34 % 3)); // this is the best way to perform function using brakets
+
+// console.log(+true);
+// console.log(+0);
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100;
+// gameCounter++; // this is a prefix increament and we can also use ++gameCounter which is postfix 
+// console.log(gameCounter);
---
+++ 0_1Basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log( 4 > 12 );
+// console.log( 2 >= 1 );
+// console.log( 2 = 1 );
+// console.log( 2 != 1 );
+
+// console.log("2" > 1); // here js automatically converted "2" into number and give the result
+// console.log("02" > 1); // always try to compare same datatypes
+
+// console.log(null > 0); // Here in all three below functions null is converting into 0 by js and hence the result
+// console.log(null == 0); // this type of conversion are needed to be avoided
+// console.log(null >= 0); 
+
+// console.log(undefined == 0); // here it throws false because undefined is not equal , equal , smaller than 0 or any number.
+
+// == and === are both different == only check the number which are comparing but === check the datatypes also
+
+// console.log("2" === 2); // here it throws false as both the datatypes are different
