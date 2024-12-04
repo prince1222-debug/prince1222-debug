1646999c989e7e6ac53393c497392e6f467c100c
prince1222-debug <boisfromhell@gmail.com>
2024-12-04 12:42:54 +0000

    num and maths

---
+++ 0_1Basics/07_num_and_maths.js
@@ -0,0 +1,34 @@
+// const scorePubg = 233
+// console.log(scorePubg)
+
+// const balanceMoney = new Number(10300);
+// console.log(balanceMoney)
+
+// console.log(balanceMoney.toString().length()); // this will convert the number into string and you can use string property in it.
+// console.log(balanceMoney.toFixed(2)); // this will print the upto 2nd place of decimal value so 10300.00
+
+// const number = 12.798
+// console.log(number.toPrecision(2)) // it will round off the number till 2nd place from starting and then give the result.
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN')); // this will result the 1000000 into 10,00,000 
+
+// Maths Library in JS//
+
+// console.log(Math);
+// console.log(Math.abs(-33)); // absolute is abs and it will only convert -ve number into +ve number
+// console.log(Math.round(23.44)); // it will round off the number 23.44 
+// console.log(Math.ceil(23.1)); // it will give the number 24 
+// console.log(Math.floor(123.33)); // it will round off 123 and if it is 123.7 it will print 123 only
+// console.log(Math.pow(2,4)); // it will give the 2 ka power 4 in result
+// console.log(Math.min(23,4,2,5,24)); // it will print the smallest number among which are given
+// console.log(Math.max(23,4,2,5,24)); // it will print the largest number among which are given
+
+// console.log(Math.random()); // it will print the randum number between 0-1
+// console.log(Math.random()*10 + 1); // it will print the randum number btw 1-10
+// console.log(Math.floor(Math.random()*10) + 1); // it will print the randum number btw 1-10
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
