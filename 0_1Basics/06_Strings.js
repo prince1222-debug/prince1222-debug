8285db9dc73aa7a94de19ec5bdb2cbdd381c0549
prince1222-debug <boisfromhell@gmail.com>
2024-12-04 11:23:39 +0000

    strings

---
+++ 0_1Basics/06_Strings.js
@@ -0,0 +1,31 @@
+// const name = "prince"
+// const gameCount = 80
+// console.log(name + "'s gameCount is " + gameCount ) This is the Older way to concatenate strings
+// You can use this below for string interpolation
+// console.log(`Hello my name is ${name} and my gamecount is ${gameCount}`) 
+
+// Below is the other way to declare string
+
+// const gameName = new String('valorant-fpp-tpp');
+
+// console.log(gameName[0]); // this will result the first letter of the valorant word
+// console.log(gameName.__proto__); // this will help you to know this string is an object
+// console.log(gameName.length); // this will result the lenght of valorant word
+
+// console.log(gameName.toUpperCase); // this will make the world valorant in uppercase and didn't change the orginal string
+// console.log(gameName.charAt(4)) // this will help you to know that which character is at index 4 or any index
+// console.log(gameName.indexOf("l")) // this will help you to know that at which index the character "l" is
+
+
+
+// console.log(gameName.substring(0, 5)) // In substring you can choose that the particular word you need to get and in this you have to use index and negative index is not allowed 
+// console.log(gameName.slice(-8, 5)) // In slice you can do as same as substring but you can choose negative index 
+
+// const newString = "   prince   "
+// console.log(newString.trim) // trim will help you to trim the empty space of the string.
+
+// const url = "https://prince%23khdfsci%23prince"
+// console.log(url.replace("%23", "-")) // using replace you can replace the string 
+// console.log(url.includes("prince")) // using includes you can know that prince is in the string and throw the result in booleans
+
+// console.log(gameName.split('-'));
