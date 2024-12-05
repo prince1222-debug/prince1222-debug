c369eb8ba238333da78b73b901da2193bb446c07
prince1222-debug <boisfromhell@gmail.com>
2024-12-05 02:44:46 +0000

    arrays1

---
+++ 0_2Basics/01_Arrays.js
@@ -0,0 +1,37 @@
+// Arrays //
+
+// const myArray = [0, 1, 2, 3, 4, 5]
+// const myArray2 = ["ravi", "rakesh", "rajender"]
+// const myArray3 = new Array("prince", "anand", "ram", "takel")
+
+// console.log(myArray[5])
+
+// Arrays Methods //
+
+// myArray.push(8) // It will add 8 at the end of your arrays
+// myArray.pop() // It will remove the last element of your arrays
+// myArray.unshift(10) // It will add 10 at the starting of the arrays
+// myArray.wshift() // It will remove the fist element of your arrays
+
+// console.log(myArray.includes(4)) // it will print true or false when 4 is in the array
+// console.log(myArray.indexOf(4)) // it will print the index of 1
+
+// const newArray = myArray.join() //it will join your myArray and make the newArray into string
+// console.log(newArray)
+// console.log(typeof newArray) // It will make your array into string
+
+// console.log(myArray)
+
+// Slice or Splice //
+
+// console.log("A " + myArray);
+
+// const mynewArray = myArray.slice(1,4) // Here in slice it will remove the element from 1 index to 3 index and not include the 4th index and it will not change the original array
+// console.log(mynewArray)
+
+// console.log("B "+ myArray);
+
+// const mynewArray2 = myArray.splice(1,4) // Here in splice it will remove the index of the element which is not included in slice and change the original array as it say splice it will remove the element which is in the splice function and give a new array of your old array 
+// console.log("C " + myArray)
+// console.log(mynewArray2)
+
